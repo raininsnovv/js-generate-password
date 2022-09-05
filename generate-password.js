081a21c139063ebe642ea155f9a4286e1acbf4d9
raininsnovv <raininsnovv@gmail.com>
2022-09-05 09:59:59 +0300

    Исправил ошибку в выводе medium-пароля

--- generate-password.js
+++ generate-password.js
@@ -4,18 +4,17 @@ function generatePassword(level, length, amount = 1) {
   let newPassword = "";
   const digits = "0123456789";
   const symbols = "!#$%&()*+,-./:;<=>?@[]^_{|}~";
-  let str = alphabet + digits + symbols;
+  let medStr = digits + alphabet;
+  let hardStr = alphabet + digits + symbols;
   for (let k = amount; k >= 1; k--) {
     for (let i = 0; i < length; i++) {
       if (level === "easy") {
         // я умею брать только первый символ из строки, а нужно взять один случайный символ :(
         newPassword += alphabet[Math.floor(Math.random() * alphabet.length)];
       } else if (level === "medium") {
-        newPassword +=
-          digits[Math.floor(Math.random() * digits.length)] +
-          alphabet[Math.floor(Math.random() * alphabet.length)];
+        newPassword += medStr[Math.floor(Math.random() * medStr.length)];
       } else if (level === "hard") {
-        newPassword += str[Math.floor(Math.random() * str.length)];
+        newPassword += hardStr[Math.floor(Math.random() * hardStr.length)];
       }
     }
     if (arrPassword.indexOf(newPassword) !== -1) {
@@ -32,4 +31,4 @@ function generatePassword(level, length, amount = 1) {
   }
 }
 
-console.log(generatePassword("hard", 8, 3));
+console.log(generatePassword("hard", 4, 5));
